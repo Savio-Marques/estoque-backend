7baef0a4c55f849708734b276cdd985b573a345d
Savio-Marques <saviomarquessouza@gmail.com>
2025-08-26 11:43:00 -0300

    v1 version config

--- src/main/java/com/marques/estoque/security/WebConfig.java
+++ src/main/java/com/marques/estoque/security/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedOrigins("https://SEU-FRONTEND-VAI-AQUI.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
