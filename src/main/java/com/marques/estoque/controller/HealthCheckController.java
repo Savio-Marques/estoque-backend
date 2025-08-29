7225d9907fb1f3f620dbd8a377de3e41efa833a2
Savio-Marques <saviomarquessouza@gmail.com>
2025-08-29 20:46:31 -0300

    feat: log info health

--- src/main/java/com/marques/estoque/controller/HealthCheckController.java
+++ src/main/java/com/marques/estoque/controller/HealthCheckController.java
@@ -1,15 +1,18 @@
 package com.marques.estoque.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
+        log.info("I'm alive!");
         return ResponseEntity.ok("I'm alive!");
     }
 }
