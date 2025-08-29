828314e5420597308f6552b03e23b9e848c0af7e
Savio-Marques <saviomarquessouza@gmail.com>
2025-08-29 00:48:26 -0300

    feat: allowed vercel

--- src/main/java/com/marques/estoque/security/WebConfig.java
+++ src/main/java/com/marques/estoque/security/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://estoque-frontend-khaki.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
